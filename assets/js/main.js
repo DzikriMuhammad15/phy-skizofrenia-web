479fdbb21e5eaeda46d7b6b2340049f907b29374
dzikrimrputra <18221144@std.stei.itb.ac.id>
2024-06-30 12:47:44 +0700

    upload code from local

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction() {
+  // Logika untuk fungsi menu
+  alert("Menu button clicked!");
+}
+
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+
+
+
+/* ----- TYPING EFFECT ----- */
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+
+
+/* -- HOME -- */
+
+
+/* -- PROJECT BOX -- */
+
+
+/* -- HEADINGS -- */
+
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+
+
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+
+
